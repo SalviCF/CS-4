b9b9abcd1f820daf5473b7af1313eb0068cf3666
Salvi <scarrillo@uma.es>
2019-09-10 14:38:02 +0200

    Minor changes

--- Scientific Computation Laboratory/Modules/7. Text Mining/Text_Mining_2.R
+++ Scientific Computation Laboratory/Modules/7. Text Mining/Text_Mining_2.R
@@ -1,27 +1,9 @@
-## ----eval=FALSE,warning=FALSE,message=FALSE------------------------------
-## Needed <- c("tm", "SnowballCC", "RColorBrewer", "ggplot2", "wordcloud",
-## "biclust", "cluster", "igraph", "fpc")
-## install.packages(Needed, dependencies=TRUE)
-## #install.packages("Rcampdf", repos = "http://datacube.wu.ac.at/",  type = "source")
-## install.packages("pdftools")
-
-## ------------------------------------------------------------------------
 library(tm)
 library(pdftools)
 library(stringr)
 library(stringi)
 
-
-## ------------------------------------------------------------------------
-# En Mac -  si son ficheros pdf (necesitas pdftotext instalado en Mac)
-# O para convertir pdf a texto es posible usar paquete R pdftools
-#library(pdfinfo)
-# 'pdfinfo' is not available (for R version 3.5.0)
-
 directorio.textos <- file.path("~", "Desktop", "texts_Alz")
-
-#On Windows
-#directorio.textos <- file.path("C:", "texts")
 
 directorio.textos
 dir(directorio.textos)
@@ -32,7 +14,6 @@ list.files <- DirSource(directorio.textos)
 # Si es un fichero de texto  
 # docs <- Corpus(DirSource(directorio.textos))
 
-
 Rpdf <- readPDF(control = list(text = "-layout"))
 
 # Crear Corpus
@@ -42,54 +23,37 @@ docs
 docs[1]
 summary(docs)
 
-
-## ------------------------------------------------------------------------
 options(stringsAsFactors = FALSE)
 
-
-## ----eval=TRUE, echo=TRUE, results='hide'--------------------------------
 #Compactar palabras, eliminar patrones, etc. 
 sub('thank you','thanks', docs,ignore.case = TRUE)
 sub('pls','please', docs,ignore.case = TRUE)
 
-
-## ----eval=TRUE, echo=TRUE------------------------------------------------
 micad <- "We'll bes sure to send Jon Katz some kudos on your behalf on tuesday to bes punished"
 micad <- sub("to","TO",micad)
 micad
 micad <- gsub("bes","be",micad)
 micad
 
-# COPIAR Y PEGAR EN LA CONSOLA
 #lista.cadenas.reemplazar <- c("We'll","Jon Katz", "tuesday")
 #lista.nueva.cadenas <- c("We will","J.K.", "Tuesday")
 #micad <- mgsub(lista.cadenas.reemplazar,lista.nueva.cadenas,micad)
 #micad
 
-
-
-## ----eval=TRUE, echo=TRUE, results='hide'--------------------------------
 #Compactar palabras, eliminar patrones, etc. 
 micad <- "We'll be sure,  we'll be, to send Jon Katz some kudos on your behalf on tuesday to bes punished"
 gsub('we\'ll','we are',micad,ignore.case = TRUE)
 
 
-## ----eval=TRUE, echo=FALSE, results='hide'-------------------------------
 gsub('[[:punct:]]','',textos$text,ignore.case = TRUE)
 
-
-## ----eval=TRUE, echo=FALSE, results='hide'-------------------------------
-# copiar y pegar en la consola
 #meses <- c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec")
 #num.meses <- 1:12
 #textos$month <- mgsub(meses,num.meses,textos$month)
 #textos$the.date <- paste(textos$date,textos$month,textos$year,sep='-')
 
-
-## ----eval=TRUE, echo=FALSE, results='hide'-------------------------------
 stri_count(head(textos$text),fixed = 'is')
 stri_detect(head(textos$text),fixed = 'is')
 # Detectar ciertos patrones
 patrones <- with(textos,str_detect(textos$text,'sorry')&str_detect(textos$text,'problem') )
 textos[patrones,5]
-
--- Scientific Computation Laboratory/Modules/7. Text Mining/Text_Mining_3.R
+++ Scientific Computation Laboratory/Modules/7. Text Mining/Text_Mining_3.R
@@ -1,4 +1,3 @@
-## ------------------------------------------------------------------------
 library(tm)
 library(pdftools)
 library(stringr)
@@ -6,16 +5,10 @@ library(stringi)
 library(ggplot2)
 library(wordcloud)
 
-
-
-## ------------------------------------------------------------------------
 textos <- read.csv("./data/oct_delta.csv")
 View(textos)
 tweets <- data.frame(doc_id=seq(1:nrow(textos)), text=textos$text)
 
-
-
-## ------------------------------------------------------------------------
 # ?tm_map
 # tm_map(documento,getTransformations)
 # ?getTransformations
@@ -38,36 +31,26 @@ return(Corpus)
   
 #  For a list of the stopwords, see: length(stopwords("english")), 
 
-
-
-## ----eval=FALSE----------------------------------------------------------
 ## stopwords("english")
 ## corpus <- clean.text(tweets$text,stopwords("english"))
 ## tweets <- tm_map(tweets$text,removeWords, stopwords("english"))
 
-
-## ------------------------------------------------------------------------
 corpus <- Corpus(VectorSource(tweets$text))
 corpus <- clean.text(corpus,stopwords("english"))
 #primer documento
 inspect(corpus[[1]])
 
-
-## ------------------------------------------------------------------------
 corpus <- tm_map(corpus,removeWords,c('aa'))
 #primer tweet tras limpieza
-# cadena aa eliminada
+#cadena aa eliminada
 inspect(corpus[[1]])
 corpus[[1]]$content
 corpus[[1]]$meta
 
-
-## ------------------------------------------------------------------------
-# Ayuda: Comando seq que se usará en un for para ir seleccionando 
+# Ayuda: Comando seq que se usar� en un for para ir seleccionando 
 # los documentos de texto
 head(seq(corpus)) 
 #[1] 1 2
-
 
 #Eliminar caracteres especiales
 for (j in seq(corpus))
@@ -93,7 +76,7 @@ corpus <- tm_map(corpus, removeWords, c("part", "someone", "aging"))
 # Esto debería funcionar. Probar. ¿En Mac, no?
 
 # tweets <- tm_map(tweets, removeWords, c("<d5>", "<d1>", "\f", "?", "<d1>"))
-# Línea anterior comentada porque estos códigos no 
+# L�nea anterior comentada porque estos c�digos no 
 # vienen como una palabra, aparecen junto a palabras
     
 # Otra posibilidad. Esto si funciona 
@@ -108,91 +91,49 @@ remove_string1 <- function(mycorpus){
   gsub("\n"," ",mycorpus);  gsub("<d5>"," ",mycorpus)
   gsub("<d1>"," ",mycorpus); gsub("\f"," ",mycorpus)
   gsub("?"," ",mycorpus); gsub("<d1>"," ",mycorpus)
-  }
-# Esto debería funcionar. Solo elimina la  primera cadena
+}
+
+# Solo elimina la  primera cadena
 corpus <- tm_map(corpus,remove_string1)
 inspect(corpus[1]);inspect(corpus[2])
 
-# Problemas con los tipos de codificación depende de tipo de equipo...
-# Otros intentos?
-
-
-
-## ------------------------------------------------------------------------
 toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
 corpus <- tm_map(corpus, toSpace, "\n")
 corpus <- tm_map(corpus, toSpace, "<d5>")
 corpus <- tm_map(corpus, toSpace, "<d1>")
 corpus <- tm_map(corpus, toSpace, "\f")
 
-#inspect(corpus[1]);inspect(corpus[2])
-
-
-
-## ----eval=FALSE----------------------------------------------------------
-## # Steam documents
-## # Stem words in a text document using Porter's stemming algorithm.
-## corpus <- tm_map(corpus, stemDocument)
-## 
-## #To treat your preprocessed documents as text documents.
-## #corpus <- tm_map(corpus, PlainTextDocument)
-## #inspect(corpus[1]);inspect(corpus[2])
-## 
-
-
-## ----eval=FALSE----------------------------------------------------------
-## tm_map(corpus,function(x) iconv(enc2utf8(x), sub ="byte"))
-## 
-
-
-## ------------------------------------------------------------------------
 dtm <- DocumentTermMatrix(corpus)
 dtm
 class(dtm)
 str(dtm)
-#If you prefer to export the matrix to Excel:
+# If you prefer to export the matrix to Excel:
 m <- as.matrix(dtm)
 dim(m)
 write.csv(m, file="dtm.csv")
 
-
-
-## ------------------------------------------------------------------------
 freq <- colSums(as.matrix(dtm))
 length(freq)
 ord <- order(freq)
 dtms <- removeSparseTerms(dtm, 0.1) 
 # This makes a matrix that is 10% empty space, maximum.
-#Word Frequency
+# Word Frequency
 freq[head(ord)]
 freq[tail(ord)]
 
-
-
-## ------------------------------------------------------------------------
 findFreqTerms(dtm, lowfreq=30)
 wf <- data.frame(word=names(freq), freq=freq)
 head(wf)
-
-
-## ------------------------------------------------------------------------
 
 p <- ggplot(subset(wf, freq>150), aes(word, freq))
 p <- p + geom_bar(stat="identity")
 p <- p + theme(axis.text.x=element_text(angle=45, hjust=1))
 p
 
-
-## ------------------------------------------------------------------------
-#?findAssocs
 AS <-findAssocs(dtm, c("can" , "pleas"), corlimit=0.2) 
 # specifying a correlation limit of 0.98
 AS$can
 AS$pleas
-
-
-
-## ------------------------------------------------------------------------
 
 set.seed(142)
 wordcloud(names(freq), freq, min.freq=25)
@@ -210,5 +151,3 @@ head(v,14)
 words <- names(v)
 d <- data.frame(word=words, freq=v)
 wordcloud(d$word,d$freq,min.freq=50)
-
-
